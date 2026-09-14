c7cf5e86143f1c350e75b124d25c518bc6550d0e
lopezu59 <juanlopezq68@gmail.com>
2026-09-14 02:09:43 -0500

    feat: agregar punto de inicio de la aplicacion

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import model.Cliente;
+import model.Vehiculo;
+import model.Entrega;
+
+import Controller.MenuController;
 
 public class Main {
-}
+
+    public static void main(String[] args) {
+
+        MenuController menuController = new MenuController();
+
+        menuController.iniciar();
+
+        }
+    }
